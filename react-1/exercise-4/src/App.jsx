cb80898260f0f2c596806319ca510b939a1fb20b
sukandasaeieo <sukandasaeieo@gmail.com>
2023-08-29 16:39:22 +0700

    react-4

--- react-1/exercise-4/src/App.jsx
+++ react-1/exercise-4/src/App.jsx
@@ -10,5 +10,49 @@ function App() {
     </div>
   );
 }
+function Header() {
+  return (
+    <head>
+    <title>Data Input and Display</title>
+  </head>
+  );
+}
+function RunningForm (){
+  return(
+    <div id="RunningForm">
+    <h1>Enter Data</h1>
+    <form id="data-form">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" /><br /><br />
 
+      <label for="age">Age:</label>
+      <input type="number" id="age" name="age" /><br /><br />
+
+      <label for="weight">Weight:</label>
+      <input type="number" id="weight" name="weight" /><br /><br />
+
+      <label for="running">Current Running (Minutes):</label>
+      <input type="number" id="running" name="running" /><br /><br />
+
+      <button type="button" onClick="addData()">Add Data</button>
+    </form>
+    </div>
+  );
+}
+function TableDisplay (){
+return(
+  <div id= "TableDisplay"> 
+<h1>Entered Data</h1>
+    <table id="data-table" border="1" width="100%">
+      <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Weight</th>
+        <th>Current Running (Minutes)</th>
+        <th>Good Running</th>
+      </tr>
+    </table>
+</div>
+);
+}
 export default App;
