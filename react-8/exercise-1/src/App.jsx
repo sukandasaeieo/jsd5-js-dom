090a2f47e77a7677f46a21ab1e7f8f6d352eb8f4
sukandasaeieo <sukandasaeieo@gmail.com>
2023-09-22 10:43:42 +0700

    add1

--- react-8/exercise-1/src/App.jsx
+++ react-8/exercise-1/src/App.jsx
@@ -13,11 +13,39 @@ function App() {
 class RegistrationForm extends React.Component {
   render() {
     return (
-      // code here
+      <div>
+      <h1>Registration Form</h1>
+      <form id="registrationForm">
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
     )
   }
 }
 
 // create Class DisplayMember here
-
+class DisplayMember extends React.Component {
+  render() {
+    //ใส่ตัวแปรใน scope render
+    const people = [
+      "John Doe (john@example.com)",
+      "Jane Smith (jane@example.com)",
+    ];
+    return (
+      <div class="members">
+      <h2>Registered Members</h2>
+      <div class="member">{people[0]}</div>
+      <div class="member">{people[1]}</div>
+    </div>
+    )
+  }
+}
 export default App;
